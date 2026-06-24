02c9170f6b3cd6d4503bbacc6f08fa0621f8d7dc
Zico Padalino <zico.padalino@untirta.ac.id>
2026-06-24 23:56:50 +0700

    push

--- deploy/infinityfree-package/htdocs/laravel/database/sql/employee_non_shift.sql
+++
@@ -1,5 +0,0 @@
--- Opsi Non Shift untuk pegawai
--- Jalankan setelah migration atau standalone di MySQL
-
-ALTER TABLE `employees`
-    ADD COLUMN `is_non_shift` TINYINT(1) NOT NULL DEFAULT 0 AFTER `shift_id`;
